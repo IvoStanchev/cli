d17b35acdfb76e77892becf80c066912d191d1c5
Gar <gar+gh@danger.computer>
2025-01-23 16:34:20 -0800

    feat(cache): add npx commands

--- tap-snapshots/test/lib/docs.js.test.cjs
+++ tap-snapshots/test/lib/docs.js.test.cjs
@@ -2677,13 +2677,16 @@ alias: issues
 `
 
 exports[`test/lib/docs.js TAP usage cache > must match snapshot 1`] = `
-Manipulates packages cache
+Manipulates packages and npx cache
 
 Usage:
 npm cache add <package-spec>
 npm cache clean [<key>]
 npm cache ls [<name>@<version>]
 npm cache verify
+npm cache npx ls
+npm cache npx rm [<key>]
+npm cache npx info [<key>]
 
 Options:
 [--cache <cache>]
@@ -2695,6 +2698,9 @@ npm cache add <package-spec>
 npm cache clean [<key>]
 npm cache ls [<name>@<version>]
 npm cache verify
+npm cache npx ls
+npm cache npx rm [<key>]
+npm cache npx info [<key>]
 \`\`\`
 
 Note: This command is unaware of workspaces.
